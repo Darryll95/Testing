a39a66e5d72972c728d7091b322bdd6a3797327f
Darryll Merkeur <darryll.merkeur@gmail.com>
2022-10-25 21:53:15 +0200

    read

---
+++ src/main/java/za/ac/cput/librarysystem/repository/PublisherRepository.java
@@ -0,0 +1,4 @@
+package za.ac.cput.librarysystem.repository;
+
+public interface PublisherRepository {
+}
---
+++ src/main/java/za/ac/cput/librarysystem/repository/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package za.ac.cput.librarysystem.repository.impl;
+
+public class UserServiceImpl {
+}
